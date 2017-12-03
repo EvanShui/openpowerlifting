d80bf7e4de543cc9272b95ed66a4385819876b6d
LucasYoung <ly.youngs@gmail.com>
2017-12-02 17:45:19 -0800

    Remove nans

--- scripts/find-outliers.py
+++ scripts/find-outliers.py
@@ -4,11 +4,14 @@
 # Prints outliers in database
 #
 
+import statistics
 import numpy as np
+from numpy import array
 import math
 import pandas as pd
 import nltk
 from nltk.cluster.kmeans import KMeansClusterer
+from nltk.cluster import euclidean_distance
 
 # TODO federation not in openpowerlifting.csv
 
@@ -17,58 +20,133 @@ from nltk.cluster.kmeans import KMeansClusterer
 
 indexes = {}
 
-# TODO normalizations
-# TODO remove null data
+norm_dict = {}
 
-age_normalize = 1
-bw_normalize = 1
-squat_normalize = 1
-bench_normalize = 1
-deadlift_normalize = 1
+labels = []
 
 NUM_CLUSTERS = 2
 REPEATS = 1
 
+def find_max(db):
+    ret_dict = {}
+    for column in db.columns:
+        try:
+            ret_dict[column] = (db[column][db[column].idxmax()])
+        except:
+            print("invalid type")
+
+    return ret_dict
+
+def serializeStr(astr):
+    if not isinstance(astr, str):
+        return 0
+
+    total = 0
+    multiplier = 1
+    for c in astr:
+        total += ord(c) * multiplier
+        multiplier *= 256
+
+    return total
+
 def get_data(path):
     db = pd.read_csv(path)
+
+    global norm_dict
+    norm_dict = find_max(db)
+
+    db.drop("Deadlift1Kg", 1, inplace=True)
+    db.drop("Deadlift2Kg", 1, inplace=True)
+    db.drop("Deadlift3Kg", 1, inplace=True)
+    db.drop("Deadlift4Kg", 1, inplace=True)
+    db.drop("McCulloch", 1, inplace=True)
+    db.drop("Place", 1, inplace=True)
+    db.drop("LifterID", 1, inplace=True)
+    db.drop("Squat1Kg", 1, inplace=True)
+    db.drop("Squat2Kg", 1, inplace=True)
+    db.drop("Squat3Kg", 1, inplace=True)
+    db.drop("Squat4Kg", 1, inplace=True)
+    db.drop("Bench1Kg", 1, inplace=True)
+    db.drop("Bench2Kg", 1, inplace=True)
+    db.drop("Bench3Kg", 1, inplace=True)
+    db.drop("Bench4Kg", 1, inplace=True)
+
+    index_remove = []
+    for col in (range(len(db))):
+        for row in range(len(db.columns)):
+            val = (db[db.columns[row]][col])
+            if not isinstance(val, str):
+                if(np.isnan(val)):
+                    index_remove.append(col)
+                    break
+    indexes_to_keep = set(range(db.shape[0])) - set(index_remove)
+    db = db.take(list(indexes_to_keep))
+
+    global labels
+    labels = db.columns
 
     for i in range(len(db.columns)):
         indexes[db.columns[i]] = i
     print(indexes)
 
-    # TODO normalize
+    # Serialize all strings in database
+    for j in range(len(db)):
+        #db.set_value(j, "Month", serializeStr(db["Month"][j]))
+        db.set_value("Name", i, serializeStr(db["Name"][i]))
+        db.set_value("Sex", i, serializeStr(db["Sex"][i]))
+        db.set_value("Event", i, serializeStr(db["Event"][i]))
+        db.set_value("Equipment", i, serializeStr(db["Equipment"][i]))
+        db.set_value("Division", i, serializeStr(db["Division"][i]))
 
-    return db.values
+    asarr = [array(f) for f in db.as_matrix()]
+    return asarr
+
+def svariate_outlier(cluster):
+    global labels
+    db = pd.DataFrame(data=cluster, columns=labels)
+    means = []
+    for key in indexes.keys():
+        means.append(statistics.mean(f for f in db[key]))
+
+    print(means)
+        #total = 0
+        #for j in cluster:
+        #    total += cluster[j][value]
+        #means.append(total / len(cluster))
+
+    for i in range(len(cluster)):
+        pass
+
 
 def euclid_component(a, b, normalization):
-    if(np.isnan(a)):
+    try:
+        if(np.isnan(a)):
+            return 0
+        if(np.isnan(b)):
+            return 0
+        return (float(a) - float(b)) ** 2 / (normalization ** 2)
+    except:
         return 0
-    if(np.isnan(b)):
-        return 0
-    return (float(a) - float(b)) ** 2 / (normalization ** 2)
-
-def find_outlier(cluster):
-    pass
 
 # u and v are vectors
 def distance_func(u, v):
 
-    if u[indexes["Sex"]] is v[indexes["Sex"]]:
+    if u[indexes["Sex"]] == v[indexes["Sex"]]:
         sex = 1
     else:
         sex = 0
 
-    if u[indexes["Event"]] is v[indexes["Event"]]:
+    if u[indexes["Event"]] == v[indexes["Event"]]:
         event = 1
     else:
         event = 0
 
-    if u[indexes["Equipment"]] is v[indexes["Equipment"]]:
+    if u[indexes["Equipment"]] == v[indexes["Equipment"]]:
         equip = 1
     else:
         equip = 0
 
-    if u[indexes["Division"]] is v[indexes["Division"]]:
+    if u[indexes["Division"]] == v[indexes["Division"]]:
         division = 1
     else:
         division = 0
@@ -77,11 +155,11 @@ def distance_func(u, v):
         # categorical variables
         sex + event + equip + division +
         # continuous variables
-        euclid_component(u[indexes["Age"]], v[indexes["Age"]], age_normalize) +
-        euclid_component(u[indexes["BodyweightKg"]], v[indexes["BodyweightKg"]], bw_normalize) +
-        euclid_component(u[indexes["BestSquatKg"]], v[indexes["BestSquatKg"]], squat_normalize) +
-        euclid_component(u[indexes["BestBenchKg"]], v[indexes["BestBenchKg"]], bench_normalize) +
-        euclid_component(u[indexes["BestDeadliftKg"]], v[indexes["BestDeadliftKg"]], deadlift_normalize)
+        euclid_component(u[indexes["Age"]], v[indexes["Age"]], norm_dict["Age"]) +
+        euclid_component(u[indexes["BodyweightKg"]], v[indexes["BodyweightKg"]], norm_dict["BodyweightKg"]) +
+        euclid_component(u[indexes["BestSquatKg"]], v[indexes["BestSquatKg"]], norm_dict["BestSquatKg"]) +
+        euclid_component(u[indexes["BestBenchKg"]], v[indexes["BestBenchKg"]], norm_dict["BestBenchKg"]) +
+        euclid_component(u[indexes["BestDeadliftKg"]], v[indexes["BestDeadliftKg"]], norm_dict["BestDeadliftKg"])
     )
 
     return distance
@@ -89,8 +167,24 @@ def distance_func(u, v):
 if __name__ == '__main__':
     import sys
 
-    kclusterer = KMeansClusterer(NUM_CLUSTERS, distance=distance_func, repeats=REPEATS)
+    kclusterer = KMeansClusterer(NUM_CLUSTERS, distance=distance_func, repeats=REPEATS, avoid_empty_clusters=True)
     data = get_data(sys.argv[1])
-    clusters = kclusterer.cluster(data, assign_clusters=True)
 
-    print(clusters)
+    print(data)
+
+    assigned_clusters = kclusterer.cluster(data, assign_clusters=True)
+
+    clusters = []
+    for i in range(NUM_CLUSTERS):
+        clusters.append([])
+
+    for i in range(len(assigned_clusters)):
+        clusters[assigned_clusters[i]].append(data[i])
+
+    print("yo")
+
+    #print(clusters[0])
+    #print(clusters[1])
+
+    #for cluster in clusters:
+        #svariate_outlier(cluster)
